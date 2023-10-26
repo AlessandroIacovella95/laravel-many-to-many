b3e0a1721527e9cda1f40f5d9237d2ffc7c53e69
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-10-26 23:30:52 +0200

    add relation many to many

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -15,10 +15,22 @@ class Project extends Model
 
     }
 
+    public function Technologies()
+    {
+        return $this->belongsToMany(Technology::class);
+    }
+
     public function getTypeBadge()
     {
         return $this->type ? "<span class='badge' style='background-color: {$this->type->color}'>{$this->type->label}</span>" : "Undefined";
+    }
 
-
+    public function getTechnologyBadges()
+    {
+        $badges = "";
+        foreach ($this->technologies as $technology) {
+            $badges .= "<span class='badge mx-1' style='background-color: {$technology->color}'>{$technology->label}</span>";
+        }
+        return $badges;
     }
 }
